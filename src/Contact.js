a3ee2683be2c36aff6a481942505643c054b8883
CookieHoodie <cookiehoodie9@gmail.com>
2023-02-14 00:05:21 +0800

    Fix favicon not showing on Github Pages && update contact link for clarity

--- src/Contact.js
+++ src/Contact.js
@@ -5,23 +5,22 @@ import Link from "@mui/material/Link";
 import { Typography } from "@mui/material";
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 
-// Wanna know more about me? Read my blogs!
 function Contact() {
   return (
     <BackgroundBox id="contact" sx={{ pb: 10 }}>
       <Container>
         <Grid container>
           <Grid item xs={12} md={6} lg={5}>
-            <Typography variant="h4">Wanna know more about me?</Typography>
+            <Typography variant="h5">Wanna know more about me?</Typography>
             <Typography
-              variant="h4"
+              variant="h5"
               sx={{
                 whiteSpace: "pre",
                 display: "flex",
                 alignItems: "center",
               }}
             >
-              Read my <Link target="_blank">blogs</Link>
+              Read my <Link target="_blank">blogs (coming soon)</Link>
               <ArrowForwardIcon fontSize="large" sx={{ ml: 2 }} />
             </Typography>
           </Grid>
