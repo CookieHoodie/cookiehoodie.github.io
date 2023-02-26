ed501762ecf5d07478aa8458bc444be284e9a18c
CookieHoodie <cookiehoodie9@gmail.com>
2023-02-26 14:33:29 +0000

    Deploying to gh-pages from @ CookieHoodie/cookiehoodie.github.io@e635605eeee0f6e54a474e8b4a2a048e53d74491 🚀

--- _next/static/dInEjfglHQjpvCfjAxh7C/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/584-d6674bc0241f91d3.js","static/css/68c1db8db3ebccbb.css","static/chunks/pages/index-1b0d74b0518175ac.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/s_7lPHSgYFkvF9ER3xmB2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/584-d6674bc0241f91d3.js","static/css/68c1db8db3ebccbb.css","static/chunks/pages/index-4b8c3c46f177920e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
