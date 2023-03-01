7d455165abd5bd4704e043352ad0523bb36df275
CookieHoodie <cookiehoodie9@gmail.com>
2023-03-01 14:44:14 +0000

    Deploying to gh-pages from @ CookieHoodie/cookiehoodie.github.io@6428130b46ca11784ca0f236605816222914ab78 🚀

--- _next/static/s_7lPHSgYFkvF9ER3xmB2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/584-d6674bc0241f91d3.js","static/css/68c1db8db3ebccbb.css","static/chunks/pages/index-4b8c3c46f177920e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
