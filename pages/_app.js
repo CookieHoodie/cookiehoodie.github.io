37fc01cc910cea66f4da9ee391094135c6f725b4
CookieHoodie <cookiehoodie9@gmail.com>
2023-04-06 18:38:13 +0800

    Add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,23 @@ export default function MyApp({ Component, pageProps }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>CH's Portfolio</title>
+        {/* Google Analytics */}
+        <script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-03S08YW4V3"
+        />
+        <script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-03S08YW4V3');
+            `,
+          }}
+        />
       </Head>
       <Component {...pageProps} />
     </>
